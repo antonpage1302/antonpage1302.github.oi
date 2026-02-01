59efdc0eced3ac3eee169b9ad72a43344ca8e87a
antonpage1302 <anton1302d.enisenko@gmail.com>
2026-02-01 11:14:10 +0300

    Update server1.py

--- server1.py
+++ server1.py
@@ -6,6 +6,32 @@ time.sleep(1)
 print("okay(: code to winasoftzippacket is 11111")
 time.sleep(3)
 input('to leave press enter')
-print("leaving...")
+print("leaving... its an saved copy files ")
 time.sleep(2)
+if rated = "1":
+  print("NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO you rated 1 ")
+if rated = "2":
+  print("okay 2")
+if rated = "3":
+  print("(: ohh its 3 normald")
+if rated = "4":
+  print("yes its 4")
+if rated = "5":
+  print("50/50 = 5 ")
+if rated = "6":
+  print("yesssssssssssssssssssssssssssssssssss")
+if rated = "67":
+  print("you Are NorMald 67")
+if rated = "7":
+  print("idk 7 norm or no")
+if rated = "8":
+  print("oh yes")
+if rated = "9":
+  print("omG 9")
+if rated = "10":
+  print("superrrrrrrrrrrr")
+else:
+  print("(:")
+time.sleep(4)
+
 
