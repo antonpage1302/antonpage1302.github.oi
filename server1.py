b507c29e81f1ac241876950add110067ab22389a
antonpage1302 <anton1302d.enisenko@gmail.com>
2026-02-01 11:04:52 +0300

    Add files via upload

---
+++ server1.py
@@ -0,0 +1,11 @@
+import time
+print("hello its my first PROJECT wow")
+rated = input("rate this please 1 or 10:")
+print("hmmm", rated)
+time.sleep(1)
+print("okay(: code to winasoftzippacket is 11111")
+time.sleep(3)
+input('to leave press enter')
+print("leaving...")
+time.sleep(2)
+
